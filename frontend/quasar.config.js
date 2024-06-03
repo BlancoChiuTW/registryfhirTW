3156449cdf44eefb59e9a74bcdc84615c2659afe
Lorex <ceo@sita.tech>
2024-06-03 09:34:20 +0800

    add public path

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -58,7 +58,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
